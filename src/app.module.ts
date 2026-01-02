ac2b7babc9122f62b82b0448f9e68acbcd55d2f6
phucdhcoder19 <dhphuc1406@gmail.com>
2026-01-02 16:39:07 +0700

    use cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TicketModule } from './ticket/ticket.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
+  imports: [TicketModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
